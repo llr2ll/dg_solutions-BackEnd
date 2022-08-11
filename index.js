0d855126da46bdb66761ba9f89906a615df64f4e
Raphael Sanseverino <76877273+llr2ll@users.noreply.github.com>
2022-08-11 16:53:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pool = require('./db.js')
     app.use(express.json())
     app.use(express.urlencoded({extended: true}));
     app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', 'https://dg-solutions-back-end.vercel.app');
       res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
     
